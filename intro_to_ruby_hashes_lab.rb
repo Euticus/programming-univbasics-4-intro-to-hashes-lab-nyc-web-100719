9facf8b1b0a12d86fc644d9a9ffba7ff08c83f3f
Jay  Stewart <jayr.stewart@live.com>
2019-09-25 16:08:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,6 @@ end
 def my_hash_creator(key, value)
   got_it = {}
   got_it[:#{key}] = #{value}
-  # return a hash that includes the key and value parameters passed into this method
 end
 
 def read_from_hash(hash, key)
